fa9df54a56b6c626b60f45b8643477312f19a171
stivencabrera3 <stiven.cabrera3@gmail.com>
2020-06-25 22:00:02 -0400

    learned formatting strings

---
+++ variables_strings/formatting_strings.py
@@ -0,0 +1,15 @@
+#You can use varibles with numbers assigned to them in strings
+
+age = 24
+
+print(f'You are {age} years old')
+
+#You can also do math with it
+
+x = 5
+
+print(f'You have {x + 1 }!')
+
+name = 'stiven'
+
+print(f'You are called {name}. It\'s nice to meet you' ) 
